70e1795c37fbc54fcb1156f6aa218caf24cadc27
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-12-17 18:47:02 -0600

    a

--- api/server.test.js
+++ api/server.test.js
@@ -31,7 +31,25 @@ describe('Auth Router', () => {
     test('responds with 201 created', async () => {
       expect(res.status).toBe(201)
     })
-    test('new user is sent back')
-      expect(res.body).toMatchObject({id:3, password: '', username: 'Frog'})
+    test('new user is sent back', async () => {
+      expect(res.body).toMatchObject({message: 'username taken'})
+    })
+  })
+
+  describe('Post /api/auth/login', () => {
+    test('logs in', async () => {
+      const res = await request(server)
+        .post('/api/auth/login')
+        .send({username: 'Frog', password: 'Ribbit'})
+
+      expect(res.body.message).toBe('welcome, Frog')
+    })
+    test('needs valid credentials', async () => {
+      const res = await request(server)
+      .post('/api/auth/login')
+      .send()
+
+      expect(res.body.message).toBe('username and password required')
+    })
   })
 })
